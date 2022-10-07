af4656179da573b86e14c14ec5614036de0517ba
Elias de Souza <elias99-souza@hotmail.com>
2022-10-07 09:52:51 -0300

    att pagina

--- src/Pages/SubPages/Projetos/index.jsx
+++ src/Pages/SubPages/Projetos/index.jsx
@@ -4,6 +4,8 @@ export default function Prejetos() {
     return (
         <div className={style.projetos}>
             <h1>Projétos</h1>
+            <h1>Página em criação...</h1>
+            <h1>Carregando...</h1>
         </div>
     )
 }
