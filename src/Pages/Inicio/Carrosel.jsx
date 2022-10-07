3f2f6c02f741e78c239e8ec5633324b415a4353a
Elias de Souza <elias99-souza@hotmail.com>
2022-10-07 08:43:51 -0300

    att carosel

--- src/Pages/Inicio/Carrosel.jsx
+++ src/Pages/Inicio/Carrosel.jsx
@@ -1,6 +1,5 @@
 import Carousel from 'react-bootstrap/Carousel';
 import style from './Inicio.module.scss'
-import binary from './Assets-slide/binary.jpg'
 import binary1 from './Assets-slide/binary1.jpg'
 import code from './Assets-slide/code.jpg'
 import code1 from './Assets-slide/code1.jpg'
@@ -12,17 +11,6 @@ function UncontrolledExample() {
         <div className={style.todos}>
             <Carousel className={style.teste} >
                 <Carousel.Item className={style.item}>
-                    <img
-                        className="d-block w-100"
-                        src={binary}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3> </h3>
-                        <p> </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src={binary1}
